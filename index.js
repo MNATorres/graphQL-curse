ca5c38da688dd574558269ea3b5e4ca1cc52f527
Federico Jure <mtri21019@gmail.com>
2023-05-22 16:04:18 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const persons = [
+    {
+        name:"Matias Torres",
+        phone:"1126328057",
+        street:"Calle frontend",
+        city:"CABA",
+        id:"111111"
+    },
+    {
+        name:"Nombre Dos",
+        phone:"38884440430",
+        street:"Calle Backend",
+        city:"Cordoba",
+        id:"222222"
+    },
+    {
+        name:"Nombre Tres",
+        phone:"3888555055",
+        street:"Calle micro",
+        city:"Canada",
+        id:"333333"
+    },
+]
